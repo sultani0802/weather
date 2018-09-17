d5d1904821031ec3ca678909abf21f099eeae5ed
sultani0802 <haamedsultani@cmail.carleton.ca>
2018-09-17 17:33:27 -0400

    Finished the first view controller

--- Weather/Controller/ViewController.swift
+++ Weather/Controller/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreLocation // Allows us to use the GPS on our iPhone
+import Alamofire // Allows use to make Networking requests
+import SwiftyJSON // Makes handling JSON objects easier
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
@@ -17,6 +19,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     // MARK: - Instance Variables
     var locationManager: CLLocationManager = CLLocationManager() // Object required to use GPS
+    var weatherDataModel: WeatherDataModel = WeatherDataModel()
     
     // MARK: - IB Outlets
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -34,26 +37,53 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.requestWhenInUseAuthorization() // Ask the user to allow us to use their location but only when the app is in use
         locationManager.startUpdatingLocation() // Start looking for the GPS coordinates of the iPhone
     }
-
+    
     
     //MARK: - Networking
     //Write the getWeatherData method here:
-    
-    
-    
-    
+    func getWeatherData(url: String, parameters: [String:String]) {
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON {
+            response in
+            if response.result.isSuccess {
+                print("Success! Weather data retrieved.")
+                
+                let weatherJSON : JSON = JSON(response.result.value!) // JSON from OpenWeatherMap
+                self.updateWeatherData(json: weatherJSON) // Pass the data to the method that will handle the JSON object returned
+                
+            } else {
+                print("Error: \(String(describing: response.result.error))")
+                self.currentCityLabel.text = "Error with connection"
+            }
+        }
+    }
     
     
     
     //MARK: - JSON Parsing
     //Write the updateWeatherData method here:
-    
+    func updateWeatherData(json: JSON){
+        if let temperature = json["main"]["temp"].double {
+            
+            weatherDataModel.temperature = Int(temperature - 273.15) // subtracting 273.15 to convert from kelvin to celcius
+            weatherDataModel.city = json["name"].string! // Grab the city from the JSON
+            weatherDataModel.condition = json["weather"][0]["id"].intValue // Grab the code for the condition in that city
+            weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition) // Set the weather icon's image filename according to the condition code
+            
+            updateUIWithWeatherData() // Update the UI
+        } else {
+            currentCityLabel.text = "Weather unavailable" // Tell the user that we couldn't retrieve the weather information
+        }
+    }
     
     
     
     
     //MARK: - UI Updates
-    //Write the updateUIWithWeatherData method here:
+    func updateUIWithWeatherData() {
+        currentCityLabel.text = weatherDataModel.city // Set the city
+        temperatureLabel.text = "\(String(weatherDataModel.temperature))°" // Set the temperature
+        weatherIconImageView.image = UIImage(named: weatherDataModel.weatherIconName) // 
+    }
     
     
     
@@ -70,16 +100,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         if let currentLocation = location {
             if currentLocation.horizontalAccuracy > 0 { // Check if the range of the GPS accuracy is valid (below 0 is an invalid value)
                 locationManager.stopUpdatingLocation() // Stop updating to conserve battery
-                print(currentLocation)
-                
-                print("logitude = \(currentLocation.coordinate.longitude), latitude = \(currentLocation.coordinate.latitude)")
+                locationManager.delegate = nil // Set the delegate to nil so that we don't get multiple requests being made to the endpoint while the stopUpdatingLocation() method finishes
                 
                 let longitude = String(currentLocation.coordinate.longitude)
                 let latitude = String(currentLocation.coordinate.latitude)
                 
                 let params : [String : String] = ["lon" : longitude, "lat" : latitude, "appid" : APP_ID]
                 
-                
+                getWeatherData(url: WEATHER_URL, parameters: params)
             }
         }
     }
