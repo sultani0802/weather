7d707731fd9da74ac23a80face3079005f4918e0
sultani0802 <haamedsultani@cmail.carleton.ca>
2018-09-17 16:13:00 -0400

    added location manager

--- Weather/Controller/ViewController.swift
+++ Weather/Controller/ViewController.swift
@@ -7,15 +7,16 @@
 //
 
 import UIKit
+import CoreLocation // Allows us to use the GPS on our iPhone
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     // MARK: - Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "f6f749e921fd7189db37b44ab0d7b273"
+    let APP_ID = "f6f749e921fd7189db37b44ab0d7b273" // Required to make API calls to OpenWeatherMap
     
     // MARK: - Instance Variables
-
+    var locationManager: CLLocationManager = CLLocationManager() // Object required to use GPS
     
     // MARK: - IB Outlets
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -27,7 +28,11 @@ class ViewController: UIViewController {
     // MARK: - View Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self // This class is the delegate for GPS library
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer // Accuracy for the GPS
+        locationManager.requestWhenInUseAuthorization() // Ask the user to allow us to use their location but only when the app is in use
+        locationManager.startUpdatingLocation() // Start looking for the GPS coordinates of the iPhone
     }
 
     
@@ -56,11 +61,34 @@ class ViewController: UIViewController {
     
     
     //MARK: - Location Manager Delegate Methods
-    //Write the didUpdateLocations method here:
-    //Write the didFailWithError method here:
+    // This method is called whenever the GPS receives a new location
+    // These locations are saved in the "locations" array and apends them to the end of the array
+    // The last location in the array is generally the most accurate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last // Grab the newest location
+        
+        if let currentLocation = location {
+            if currentLocation.horizontalAccuracy > 0 { // Check if the range of the GPS accuracy is valid (below 0 is an invalid value)
+                locationManager.stopUpdatingLocation() // Stop updating to conserve battery
+                print(currentLocation)
+                
+                print("logitude = \(currentLocation.coordinate.longitude), latitude = \(currentLocation.coordinate.latitude)")
+                
+                let longitude = String(currentLocation.coordinate.longitude)
+                let latitude = String(currentLocation.coordinate.latitude)
+                
+                let params : [String : String] = ["lon" : longitude, "lat" : latitude, "appid" : APP_ID]
+                
+                
+            }
+        }
+    }
     
-    
-    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error) // Print error to the console
+        
+        currentCityLabel.text = "Location unavailable" // Show the user we can't find their location
+    }
     
     
     //MARK: - Change City Delegate methods
