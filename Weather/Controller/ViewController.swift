33cbf574b6de80c960de80d277b91d0ebf2289e4
sultani0802 <haamedsultani@cmail.carleton.ca>
2018-09-17 00:11:03 -0400

    added images assets, set up segue, added some UI elements continue adding Viewcontroller.swift UI elements, continue with "weather condition image view"

---
+++ Weather/Controller/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Haamed Sultani on 2018-09-16.
+//  Copyright © 2018 Sultani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Constants
+    /***************************************************************/
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = ""
+    
+    // MARK: - Instance Variables
+    /***************************************************************/
+
+    
+    // MARK: - IB Outlets
+    /***************************************************************/
+
+    
+    // MARK: - View Methods
+    /***************************************************************/
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    //MARK: - Networking
+    /***************************************************************/
+    //Write the getWeatherData method here:
+    
+    
+    
+    
+    
+    
+    
+    //MARK: - JSON Parsing
+    /***************************************************************/
+    //Write the updateWeatherData method here:
+    
+    
+    
+    
+    
+    //MARK: - UI Updates
+    /***************************************************************/
+    //Write the updateUIWithWeatherData method here:
+    
+    
+    
+    
+    
+    
+    //MARK: - Location Manager Delegate Methods
+    /***************************************************************/
+    //Write the didUpdateLocations method here:
+    //Write the didFailWithError method here:
+    
+    
+    
+    
+    
+    //MARK: - Change City Delegate methods
+    /***************************************************************/
+    //Write the userEnteredANewCityName Delegate method here:
+    //Write the PrepareForSegue Method here
+    
+    
+    
+    
+    
+}
+
+
